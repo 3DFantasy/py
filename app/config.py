5f08a574e12a51431a4bf7f6233ce7c42965d8f6
Wilson Birch <wilsonbirch@gmail.com>
2025-04-11 22:04:38 -0400

    added config/settings

---
+++ app/config.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    NODE_ENV: str
+    APP_SECRET: str
+    REDIS_USERNAME: str
+    REDIS_QUEUE: str
+    REDIS_HOST: str
+    REDIS_PASSWORD: str
+    REDIS_PORT: str
+    DATABASE_URL: str
+    TEAM_1_URL: str
+    TEAM_2_URL: str
+    TEAM_3_URL: str
+    TEAM_4_URL: str
+    TEAM_5_URL: str
+    TEAM_6_URL: str
+    TEAM_7_URL: str
+    TEAM_8_URL: str
+    TEAM_9_URL: str
+    PXP_API_URL: str
+    FLY_WORKER_TOKEN: str
+    TEST:str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
